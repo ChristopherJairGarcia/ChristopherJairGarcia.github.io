e094c1453df19ee87df0e42382e1449e863dc1a2
xochitlortega <40500918+xochitlortega@users.noreply.github.com>
2019-07-05 11:04:43 -0700

    Add files via upload

---
+++ about.js
@@ -0,0 +1,117 @@
+@media only screen and 
+	(min-width:300px)and (max-width:425px)and (orientation:portrait)
+{
+	body{
+		background-color:ghostwhite;
+	}
+	nav a {
+    list-style-type:none;
+    margin:45;
+    overflow:hidden;
+    font-family:Merriweather;
+    background-color:#930707;
+    text-align:center;
+    float:left;
+    display:block;
+    text-align:center;
+    text-decoration:none;
+    padding:11px;
+    color:white;
+    width:100%;
+	height:100%;
+	font-size:17px;
+   	}
+	
+	p{
+	font-size: 10px;
+	font-family: 'Merriweather', serif;
+	width:100%;
+	float:left;
+	text-align:left;
+	color:black;
+	
+	}
+	img{
+		float:left;
+		width:196px;
+		height:155px;
+		width:100%;
+		height:100%
+	}
+	
+}
+
+
+
+
+
+@media only screen and
+	(min-width:667px){
+		h{
+	font-size: 30px;
+	color:black;
+	float:left;
+	font-family: 'Merriweather', serif;
+	width:100%
+}
+
+
+
+
+p{
+	font-size: 20px;
+	font-family: 'Merriweather', serif;
+	width:600px;
+	float:left;
+	color:black;
+	
+}
+
+nav{
+	font-size:10px;
+}
+
+	
+	
+}
+
+* {
+    box-sizing: border-box;
+    margin: 0px;
+    padding: 0px;
+    border: 0px;
+}
+
+body {
+    background-color: ghostwhite;
+    color: rgb(255, 255, 255);
+}
+
+nav {
+    width: 100%;
+}
+
+nav a {
+    list-style-type:none;
+    margin:45;
+    overflow:hidden;
+    font-family:Merriweather;
+    background-color:#930707;
+    text-align:center;
+    float:left;
+    display:block;
+    text-align:center;
+    text-decoration:none;
+    padding:14px;
+    color:white;
+    width: 25%;
+	font-size:25px;
+    
+		
+}
+		#okay{
+			position: relative;
+			right: 520px;
+		}
+		
+
