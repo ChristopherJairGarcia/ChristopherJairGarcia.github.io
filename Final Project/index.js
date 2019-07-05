a00f1c50072f9c0f2c0896f1f64c102f11a19b56
xochitlortega <40500918+xochitlortega@users.noreply.github.com>
2019-07-05 11:07:52 -0700

    Add files via upload

---
+++ Final Project/index.js
@@ -0,0 +1,10 @@
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+function setup(){
+    
+}
+function draw(){
+   
+    
+}
