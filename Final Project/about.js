b0ddd0459254eb52419c3651a097a8a2d311e296
NEVID05 <40500969+NEVID05@users.noreply.github.com>
2019-07-09 08:57:04 -0700

    Update about.js

--- Final Project/about.js
+++ Final Project/about.js
@@ -1,7 +1,12 @@
-function setup(){
-    
+function setup()
+{   
+    createCanvas(800, 400);
+    //sets units of angles to be in degrees for arc
+    angleMode(DEGREES);
+  
 }
-function draw(){
-   
-    
+function draw () {
+ fill(28,27,28 )
+ rect(30, 20, 655, 295);
+
 }
