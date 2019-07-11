7223cb271cca6de73ed6ee50b25f6e16ca078153
NEVID05 <40500969+NEVID05@users.noreply.github.com>
2019-07-11 08:43:19 -0700

    Update about.js

--- Final Project/about.js
+++ Final Project/about.js
@@ -9,43 +9,47 @@ function setup()
 function draw () {
     //table color
  fill( 51 );
- rect(30, 20, 755, 395);
+ rect(30, 20, 655, 295);
  //table cloth
  strokeWeight(4);
 stroke(247, 248, 250);
 fill(145, 36, 181);
-rect(80,50,550,230);
+rect(80,50,550,230);  
+
+  
+    
 
 
  //middle plate on the left
  fill( 247, 248, 250);
- ellipse(245 , 275, 55 ,55 );
+ ellipse(145 , 175, 55 ,55 );
  //top left plate 
  fill( 247, 251, 250);
- ellipse(345 , 200, 55 ,55 );
+ ellipse(245 , 100, 55 ,55 );
  //top middle plate
   fill( 247, 251, 250);
- ellipse(445 , 200, 55 ,55 );
+ ellipse(345 , 100, 55 ,55 );
  //top right plate 
   fill( 247, 251, 250);
- ellipse(550 , 200, 55 ,55 );
-  //big oval in the middle 
+ ellipse(450 , 100, 55 ,55 );
+ //big oval in the middle 
   fill( 247, 248, 250);
-  ellipse(450 , 265, 115 ,53 );
- 
+  ellipse(350 , 165, 115 ,53 );
  //middle right plate 
   fill( 247, 251, 250);
- ellipse(675 , 275, 55 ,55 );
+ ellipse(575 , 175, 55 ,55 );
  // bottom right plate
   fill( 247, 241, 250);
- ellipse(550 , 330, 55 ,55 );  
+ ellipse(450 , 230, 55 ,55 );  
  // bottom middle plate 
  fill( 247, 251, 250);
- ellipse(445 , 330, 55 ,55 );
+ ellipse(345 , 230, 55 ,55 );
  // bottom left plate 
   fill( 247, 251, 250);
- ellipse(345 , 330, 55 ,55 );
+ ellipse(245 , 230, 55 ,55 );
  
  }
  
-
+ function mouseReleased() {
+	loop();
+}
