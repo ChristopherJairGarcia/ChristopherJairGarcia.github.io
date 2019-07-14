10153ffbc6c273804aca8962657d0dfa68b6b568
NEVID05 <40500969+NEVID05@users.noreply.github.com>
2019-07-14 11:36:02 -0700

    Update about.js

--- Final Project/about.js
+++ Final Project/about.js
@@ -19,40 +19,82 @@ rect(150,120,550,230);
     
 
 
+
  //middle plate on the left
  fill( 247, 248, 250);
- ellipse(215 , 250, 55 ,55 );
+ ellipse(215 , 240, 55 ,55 );
+    if (mousepressed &&  mouseX > 160 && mouseX < 270 && mouseY >185  && mouseY < 295  )
+    {
+     fill( 247, 251, 250);
+ ellipse(325 , 295, 450, 400 );
+        
+    }
+    
  //top left plate 
  fill( 247, 251, 250);
  ellipse(325 , 175, 55 ,55 );
+     if (mousepressed &&  mouseX > 585 && mouseX < 695 && mouseY > 120 && mouseY <230   )
+     {
+     fill( 247, 251, 250);
+     ellipse(325 , 295, 450, 400 );
+     }
  //top middle plate
   fill( 247, 251, 250);
  ellipse(435 , 175, 55 ,55 );
+    if (mousepressed &&  mouseX >120  && mouseX < 230 && mouseY > 380 && mouseY < 490  )
+    {
+     fill( 247, 251, 250);
+ ellipse(325 , 295, 450, 400 );
+    }
  //top right plate 
   fill( 247, 251, 250);
  ellipse(540 , 175, 55 ,55 );
+     if (mousepressed &&  mouseX > 485 && mouseX < 595 && mouseY > 120 && mouseY < 230 )
+         
+     {
+     fill( 247, 251, 250);
+ ellipse(325 , 295, 450, 400 );
+     }
  //big oval in the middle 
   fill( 247, 238, 250);
   ellipse(440 , 235, 115 ,53 );
+  
  //middle right plate 
   fill( 247, 251, 250);
- ellipse(640 , 250, 55 ,55 );
+ ellipse(640 , 240, 55 ,55 );
+    if (mousepressed &&  mouseX > 585 && mouseX < 695 && mouseY >185  && mouseY < 295  )
+    {
+     fill( 247, 251, 250);
+ ellipse(325 , 295, 450, 400 );
+    }
  // bottom right plate
   fill( 247, 341, 250);
- ellipse(540 , 295, 55 ,55 );  
+ ellipse(540 , 295, 55 ,55 ); 
+    if (mousepressed &&  mouseX > 485 && mouseX < 595 && mouseY > 240 && mouseY < 350 )
+    {
+     fill( 247, 251, 250);
+ ellipse(325 , 295, 450, 400 );
+    }
  // bottom middle plate 
  fill( 247, 251, 250);
  ellipse(435 , 295, 55 ,55 );
+    if (mousepressed &&  mouseX >380&& mouseX <490 && mouseY > 240 && mouseY < 350 )
+    {
+     fill( 247, 251, 250);
+ ellipse(325 , 295, 450, 400 );
+    }
  // bottom left plate 
   fill( 247, 251, 250);
  ellipse(325 , 295, 55 ,55 );
  
+    if (mousepressed &&  mouseX >270 && mouseX <380 && mouseY > 240 && mouseY < 350 )
+    {
+     fill( 247, 251, 250);
+ ellipse(325 , 295, 450, 400 );
+    
+    
+}
  }
- 
- function mouseReleased() {
-	loop();
+function mouseClicked (){
+    mousepressed = true;
 }
-function d(){
-	createP(" Hi World ");
-}
-
